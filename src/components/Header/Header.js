fe5d956cdd7d8e15404d6264f362d1e71ba2ab3e
Rob Morgan <rmorgan323@gmail.com>
2018-04-18 15:58:34 -0600

    Add header component with styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
 
   return (
     <div className="Header">
-      <h5>Resume</h5>
-      <a href="https://www.linkedin.com/in/robert-a-morgan">
+      <a className="header-left header-left-resume" href="https://www.turing.io/sites/default/files/resumes/resume-3-3-18.pdf">
+        <h5>Resume</h5>
+      </a>
+      <a className="header-left header-left-linkedin" href="https://www.linkedin.com/in/robert-a-morgan">
         <h5>LinkedIn</h5>
       </a>
-      <a href="https://github.com/rmorgan323">
+      <a className="header-left header-left-github" href="https://github.com/rmorgan323">
         <h5>GitHub</h5>
       </a>
       <h1><span className="logo-left">ROB</span>ERTO</h1>
